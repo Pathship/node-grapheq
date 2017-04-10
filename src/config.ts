c1dc75e0281dcd52bf282cba5ff573677d888d29
Zachary Nevin <zachary@pathship.com>
2017-04-10 21:50:03 +0800

    Break if outside n_frames to process

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const GRAPHEQ_API_URL  = 'http://localhost:5000'
+export const GRAPHEQ_API_URL  = 'https://api.grapheq.com'
 export const GRAPHEQ_AUDIENCE = 'https://api.grapheq.com'
--- test/test.js
+++ test/test.js
@@ -1,11 +1,16 @@
+let crypto = require('crypto')
+let Grapheq = require('../index.js')
+
 let apiKey = '0aOVjtw6iGgs3g7nI923r9RmpGcH5htf'
 let apiSecret = 'rjFv6rUlDG4h5EYQ6KvhaBHh-uQ938NIdve8uzBzQfKoCuUUm8I3i3HSg9tKVCzq'
-
-let Grapheq = require('../index.js')
 let grapheq = new Grapheq(apiKey, apiSecret)
 
-console.log(grapheq)
-grapheq.tasks.start('bill.mp4')
+let videoPath = 'bill.mp4'
+let videoPathSignature = crypto.createHmac('sha256', 'P6ye3DXM2PHiQ6l8wJ5M2Ge9iTNawKli').update(videoPath).digest('hex')
+
+console.log(videoPathSignature)
+
+grapheq.tasks.start(videoPath, videoPathSignature)
   .then((task) => {
     console.log(task)
   })
